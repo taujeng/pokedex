af42f8d7ee36b7af6d6b40486decd7546475222a
Tau <taujeng.1@gmail.com>
2022-06-23 21:41:53 +0800

    General structure of Header Component

---
+++ pokedex/src/components/Header/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './header.css';
+
+export const Header = () => {
+  return (
+    <header className="header-container">
+      {/* Logo  */}
+      <img src="./images/pokedex_logo.png" alt="Pokedex Logo" id="mainLogo" />
+      {/* Captured Pokemon Button */}
+      <button className="header-button">
+        <img
+          src="./images/pokeball.png"
+          alt="Pokeball"
+          className="header-pokeball-img"
+        />
+        Captured Pokemon
+      </button>
+    </header>
+  );
+};
