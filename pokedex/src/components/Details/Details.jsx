d40e08fb26c69fbadfc902ea899f27d8cf61cc3a
Tau <taujeng.1@gmail.com>
2022-06-30 11:25:27 +0800

    Details barebones now rendering!

--- pokedex/src/components/Details/Details.jsx
+++ pokedex/src/components/Details/Details.jsx
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from 'react';
+import './details.css';
 
 const Details = ({ actualData, loading, chosenId }) => {
-  const [detailsData, setDetailsData] = useState('');
+  // const [detailsData, setDetailsData] = useState('');
   // console.log(actualData, chosenId);
 
-  useEffect(() => {
-    const whatif = actualData[chosenId];
-    setDetailsData(whatif);
-  }, []);
+  // Problem with wrapping everything in a useEffect is that
+  // would have to use useState to access things
+  // and first render the useState is empty -> giving us errors
 
   // useEffect(() => {
   //   const getDetailsData = data.filter((item) => {
@@ -16,14 +16,56 @@ const Details = ({ actualData, loading, chosenId }) => {
   //   setDetailsData(getDetailsData);
   // }, []);
 
-  // const whatif = actualData[chosenId];
-  // setDetailsData(whatif);
+  // const nameof = upperCase(whatif.name);
+  // // setDetailsData(whatif);
   // console.log(whatif, whatif.name);
+  // Uppercase Words
+  function upperCase(str) {
+    return str[0].toUpperCase() + str.slice(1);
+  }
+
+  const detailsData = actualData[chosenId];
+
+  // Once Data is received:
+  if (actualData.length > 0) {
+    // Creating Post Title
+    let postTitle = String(detailsData.id);
+    while (postTitle.length < 3) {
+      postTitle = '0' + postTitle;
+    }
+    let name1 = detailsData.name;
+    // Might have to change this if Pokemon names contain >1 word
+    name1 = upperCase(name1);
+    postTitle = '#' + postTitle + ' ' + name1;
+
+    return (
+      <div className="details-container">
+        <div className={`details-cover ${detailsData.types[0].type.name}-type`}>
+          <img
+            src={
+              detailsData['sprites']['other']['official-artwork'][
+                'front_default'
+              ]
+            }
+            alt={detailsData.name}
+            className="detail-img"
+          />
+          <h1>{postTitle}</h1>
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div>
       <h1>Detailed view</h1>
-      <h1>{actualData.order}</h1>
+
+      {/* {actualData.length > 0 && <h1>{actualData[0].name}</h1>}
+      {actualData.length > 0 && <h1>{nameof}</h1>} */}
+
+      {/* {actualData.length > 0 && <h2>{whatif.name}</h2>} */}
+      {/* <h1>{actualData.order}</h1> */}
+
       {/* --> app runs, but above isn't rendered */}
 
       {/* <h1>{whatif.name}</h1> */}
