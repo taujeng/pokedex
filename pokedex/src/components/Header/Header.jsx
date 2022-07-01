e0619915dfdff69404d80aaa5a07e6feff9ca4ca
Tau <taujeng.1@gmail.com>
2022-07-02 00:06:58 +0800

    Add notes

--- pokedex/src/components/Header/Header.jsx
+++ pokedex/src/components/Header/Header.jsx
@@ -3,6 +3,9 @@ import './header.css';
 import { Link } from 'react-router-dom';
 
 const Header = ({ removeDetails, actualData, localStorage }) => {
+  // removeDetails: on Click, stop showing Details
+  // actualData and localStorage are passed to CapturedPokemon via Link's State
+
   return (
     <div className="header-container" onClick={() => removeDetails()}>
       {/* Logo  */}
